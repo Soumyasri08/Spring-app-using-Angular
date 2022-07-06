b0c2e80611684afc55115de1b6c2aee3d03fa7e2
Soumya Sri <70061578+Soumyasri08@users.noreply.github.com>
2022-07-06 00:49:29 -0500

    Update employee.ts

--- employeemanagerapp-master/employeemanagerapp-frontend/src/app/employee.ts
+++ employeemanagerapp-master/employeemanagerapp-frontend/src/app/employee.ts
@@ -1,4 +1,9 @@
+// We are creating an interface to specify the type of data
+//that requests are going to return
 export interface Employee {
+  
+  //these are the attributes
+  
   id: number;
   name: string;
   email: string;
