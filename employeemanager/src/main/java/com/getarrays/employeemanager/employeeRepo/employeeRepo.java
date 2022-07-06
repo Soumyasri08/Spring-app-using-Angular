bffb78d262de77e9a4ff432ca5670fc89f3e449d
Soumya Sri <70061578+Soumyasri08@users.noreply.github.com>
2022-07-06 01:58:44 -0500

    Update employeeRepo.java

--- employeemanager/src/main/java/com/getarrays/employeemanager/employeeRepo/employeeRepo.java
+++ employeemanager/src/main/java/com/getarrays/employeemanager/employeeRepo/employeeRepo.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+//This class extends JPA repository which perform operations like save,find,find all etc..
+
+//The Java Persistence API (JPA) is the standard way of persisting Java objects into relational databases.it contains API for basic CRUD operations
 
 public interface employeeRepo extends JpaRepository<Employee, Long> {
+ 
+ //Since the repo doesn't contain a method like deleteEmployeeById we created a method by passing Id as argument, similarly findemployee.
  void deleteEmployeeById(Long id);
 
     Optional<Employee> findEmployeeById(Long id);
